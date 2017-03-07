6e0e51621dd2e01040ece9738dab67519f1bbfb0
Tiffany Hyatt <Hyatt@Tiffanys-MacBook-Pro.local>
2017-03-07 18:42:43 -0500

    Crystals Game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,21 +6,24 @@ var userTotalScore = 0;
 var winsArray = [];
 var lossesArray = [];
 
+//Resetting the Game function//
 function reset() {
 	userTotalScore = 0;
 }
 
+//Running the start function as soon as the document is ready, onload//
 $( document ).ready(function() {
 	start();
 });
 
+//When the users clicks on any of the ceystals a random number will be added to the users' total score and added to the score number html element//
 $("#crystals").on("click", function (){
 	userTotalScore = userTotalScore + crystalPoints;
 	$("#score-number").html(" " + userTotalScore);
 	initGame();
 })
 
-
+//Choosing a random number at start of the game and if the users' total score is equal to random number, the wins will increase by one, if not the losses will decrease bye one//
 function initGame() {
 	$("#random-number").html("Random Number: " + randomNumber);
 	if (userTotalScore === randomNumber) {
